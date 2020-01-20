405e357c8fd648d96b0b46d8ce2b2f51baefdc27
Neil <neilmichaelfranks@gmail.com>
2020-01-19 23:32:47 -0700

    done with heroku stuff please?

--- ramshackle/ramshackle/settings.py
+++ ramshackle/ramshackle/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -116,6 +118,10 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+# Change 'default' database configuration with $DATABASE_URL.
+DATABASES["default"].update(dj_database_url.config(
+    conn_max_age=500, ssl_require=True))
 
 
 # Static files (CSS, JavaScript, Images)
