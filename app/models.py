a9896ca25ee8f7cbe648bda0d72ad70af21fb740
Neil <neilmichaelfranks@gmail.com>
2020-01-20 22:22:54 -0700

    owner on models

--- app/models.py
+++ app/models.py
@@ -1,11 +1,18 @@
 from django.db import models
-
-# Create your models here.
+from django.contrib.auth.models import User
 
 
 class Prompt(models.Model):
     text = models.TextField()
+    score = models.IntegerField(default=0)  # for +1-ing good prompts
+    owner = models.ForeignKey(
+        User, related_name="prompt", on_delete=models.CASCADE, null=True
+    )
 
 
 class Answer(models.Model):
     text = models.TextField()
+    votes = models.IntegerField(default=0)
+    owner = models.ForeignKey(
+        User, related_name="answer", on_delete=models.CASCADE, null=True
+    )
